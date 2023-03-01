c3200557c18084981d9dea7c5871d63250c89824
Tim Eitel <timeitel@outlook.com>
2023-03-01 20:49:29 +0800

    feat: 1.1

--- 2022/rust/day_01/src/main.rs
+++ 2022/rust/day_01/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+    let mut max: u32 = 0;
+    let mut counter: u32 = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        if line.is_empty() {
+            if counter > max {
+                max = counter;
+            }
+            counter = 0;
+        } else {
+            counter += line.parse::<u32>().unwrap();
+        }
+    }
+
+    println!("Max is: {}", max);
+    Ok(())
 }
