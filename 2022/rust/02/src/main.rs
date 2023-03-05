5510020fe7c663ea3b93c3f6b7461433fba65d1d
Tim Eitel <timeitel@outlook.com>
2023-03-05 14:05:48 +0800

    feat: day 2 ex 2!

--- 2022/rust/02/src/main.rs
+++ 2022/rust/02/src/main.rs
@@ -1,28 +1,28 @@
 use std::fs;
 
-fn get_round_score(elf_move: &str, my_move: &str) -> Result<u32, String> {
-    let shape_score = match my_move {
-        "X" => 1,
-        "Y" => 2,
-        "Z" => 3,
+fn get_round_score(elf_move: &str, required_result: &str) -> Result<u32, String> {
+    let shape_score = match (elf_move, required_result) {
+        ("A", "X") => 3,
+        ("A", "Y") => 1,
+        ("A", "Z") => 2,
+
+        ("B", "X") => 1,
+        ("B", "Y") => 2,
+        ("B", "Z") => 3,
+
+        ("C", "X") => 2,
+        ("C", "Y") => 3,
+        ("C", "Z") => 1,
         _ => return Err("Invalid my_move".to_string()),
     };
 
-    let outcome_score = match (elf_move, my_move) {
-        ("A", "X") => 3,
-        ("A", "Y") => 6,
-        ("A", "Z") => 0,
-
-        ("B", "X") => 0,
-        ("B", "Y") => 3,
-        ("B", "Z") => 6,
-
-        ("C", "X") => 6,
-        ("C", "Y") => 0,
-        ("C", "Z") => 3,
+    let outcome_score = match required_result {
+        "X" => 0, //lose
+        "Y" => 3, //draw
+        "Z" => 6, //win
         _ => {
             return Err(format!(
-                "Invalid outcome for elf move: {elf_move} and my_move: {my_move}"
+                "Invalid outcome for elf move: {elf_move} and result: {required_result}"
             ))
         }
     };
