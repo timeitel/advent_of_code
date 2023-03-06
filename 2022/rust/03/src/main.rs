aa5a3fe62c98c20d5c5f348100bfd4326ae605e0
Tim Eitel <timeitel@outlook.com>
2023-03-06 23:21:25 +0800

    feat(03-02): how did i do that

--- 2022/rust/03/src/main.rs
+++ 2022/rust/03/src/main.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-fn get_common_char_value(first: &str, second: &str) -> u32 {
+fn get_common_char_value(first: &str, second: &str, third: &str) -> u32 {
     let alpha_lower = ('a'..='z').into_iter().collect::<Vec<char>>();
     let alpha_upper = ('A'..='Z').into_iter().collect::<Vec<char>>();
     let mut value: u32 = 0;
 
     first.chars().for_each(|char| {
-        if second.contains(char) {
+        if second.contains(char) && third.contains(char) {
             if char.is_lowercase() {
                 value = u32::try_from(
                     alpha_lower
@@ -60,5 +60,10 @@ fn main() {
         });
     }
 
-    println!("{:?}", elf_groups.pop().unwrap().bags);
+    let result = elf_groups
+        .into_iter()
+        .map(|group| get_common_char_value(&*group.bags[0], &*group.bags[1], &*group.bags[2]))
+        .sum::<u32>();
+
+    println!("{}", result);
 }
