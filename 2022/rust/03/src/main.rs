a749330b049741cb7792ae60d8e51b139c87d139
Tim Eitel <timeitel@outlook.com>
2023-03-07 23:15:53 +0800

    rf: 03-02

--- 2022/rust/03/src/main.rs
+++ 2022/rust/03/src/main.rs
@@ -1,67 +1,27 @@
+use itertools::Itertools;
+use std::collections::HashMap;
 use std::fs;
-
-fn get_common_char_value(first: &str, second: &str, third: &str) -> u32 {
-    let alpha_lower = ('a'..='z').into_iter().collect::<Vec<char>>();
-    let alpha_upper = ('A'..='Z').into_iter().collect::<Vec<char>>();
-    let mut value: u32 = 0;
-
-    first.chars().for_each(|char| {
-        if second.contains(char) && third.contains(char) {
-            if char.is_lowercase() {
-                value = u32::try_from(
-                    alpha_lower
-                        .iter()
-                        .position(|&b| b == char)
-                        .expect("Lower not working?"),
-                )
-                .unwrap()
-                    + 1;
-                println!("value is :{value} for {char}")
-            } else {
-                value = u32::try_from(
-                    alpha_upper
-                        .iter()
-                        .position(|&b| b == char)
-                        .expect("Upper not working?"),
-                )
-                .unwrap()
-                    + 27;
-                println!("value is :{value} for {char}")
-            }
-        }
-    });
-
-    value
-}
-
-struct Group<'a> {
-    bags: Vec<&'a str>,
-}
-
-impl<'a> Group<'a> {
-    fn add_bag(&mut self, bag: &'a str) {
-        self.bags.push(bag);
-    }
-}
 
 fn main() {
     let file = fs::read_to_string("input.txt").unwrap();
-    let mut elf_groups: Vec<Group> = vec![];
-
-    for (i, bag) in file.lines().enumerate() {
-        if i % 3 != 0 {
-            let len = elf_groups.len();
-            elf_groups[len - 1].add_bag(bag);
-            continue;
-        }
-
-        elf_groups.push(Group { bags: vec![bag] });
-    }
-
-    let result = elf_groups
+    let letter_scores = ('a'..='z')
+        .chain('A'..='Z')
         .into_iter()
-        .map(|group| get_common_char_value(group.bags[0], group.bags[1], group.bags[2]))
-        .sum::<u32>();
+        .enumerate()
+        .map(|(i, char)| (char, i + 1))
+        .collect::<HashMap<char, usize>>();
+
+    let result = file
+        .lines()
+        .tuples::<(_, _, _)>()
+        .map(|(bag_a, bag_b, bag_c)| {
+            let common_char = bag_a
+                .chars()
+                .find(|ch| bag_b.contains(*ch) && bag_c.contains(*ch))
+                .unwrap();
+            letter_scores.get(&common_char).unwrap()
+        })
+        .sum::<usize>();
 
     println!("{}", result);
 }
