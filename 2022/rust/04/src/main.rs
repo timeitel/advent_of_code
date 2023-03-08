e5495c5f3426567bf4b5d3d4aa404309bd516b0e
Tim Eitel <timeitel@outlook.com>
2023-03-08 17:27:37 +0800

    rf(04-02): use filter and count

--- 2022/rust/04/src/main.rs
+++ 2022/rust/04/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 
     let result = file
         .lines()
-        .map(|pair| {
+        .filter(|pair| {
             let (one, two) = pair.split_once(",").unwrap();
             let (one_lower_str, one_upper_str) = one.split_once("-").unwrap();
             let (two_lower_str, two_upper_str) = two.split_once("-").unwrap();
@@ -15,15 +15,12 @@ fn main() {
             let two_lower = two_lower_str.parse::<u32>().unwrap();
             let two_upper = two_upper_str.parse::<u32>().unwrap();
 
-            if (one_lower >= two_lower && one_upper <= two_upper)
-                || (two_lower >= one_lower && two_upper <= one_upper)
-            {
-                1
-            } else {
-                0
-            }
+            (one_lower..=one_upper).contains(&two_lower)
+                || (one_lower..=one_upper).contains(&two_upper)
+                || (two_lower..=two_upper).contains(&one_lower)
+                || (two_lower..=two_upper).contains(&one_upper)
         })
-        .sum::<u32>();
+        .count();
 
     println!("{result}");
 }
