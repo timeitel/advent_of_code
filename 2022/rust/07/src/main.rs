584b8d557f8a01fad66b16de84badb0f07d11b69
Tim Eitel <timeitel@outlook.com>
2023-03-11 14:40:27 +0800

    feat(07): part 2!!!!!

--- 2022/rust/07/src/main.rs
+++ 2022/rust/07/src/main.rs
@@ -24,7 +24,7 @@ fn update_present_and_parent_dir_sizes(
     // dbg!(&dirs);
 }
 
-fn process(file: &str) -> u32 {
+fn process(file: &str) -> (u32, u32) {
     const MAX_DIR_SIZE: u32 = 100000;
     let (_, cmds) = parse_commands(file).unwrap();
 
@@ -35,6 +35,7 @@ fn process(file: &str) -> u32 {
         match c {
             Command::Cd(Cd::Root) => {
                 pwd.clear();
+                pwd.push("/");
             }
             Command::Cd(Cd::Up) => {
                 pwd.pop();
@@ -49,7 +50,8 @@ fn process(file: &str) -> u32 {
         }
     }
 
-    let total_size = dirs.iter().fold(0, |acc, (_, size)| {
+    // part 1
+    let part_1_result = dirs.iter().fold(0, |acc, (_, size)| {
         if *size <= MAX_DIR_SIZE {
             acc + *size
         } else {
@@ -57,13 +59,32 @@ fn process(file: &str) -> u32 {
         }
     });
 
-    total_size
+    // part 2
+    let total_size = 70_000_000;
+    let update_size = 30_000_000;
+    let used_size = dirs.get("/").unwrap();
+    let available_size = total_size - used_size;
+    let required_size_to_delete = update_size - available_size;
+
+    let mut valid_sizes: Vec<u32> = dirs
+        .into_iter()
+        .filter_map(|(_, size)| {
+            if size >= required_size_to_delete {
+                Some(size)
+            } else {
+                None
+            }
+        })
+        .collect();
+    valid_sizes.sort();
+
+    (part_1_result, valid_sizes[0])
 }
 
 fn main() {
     let file = fs::read_to_string("input.txt").unwrap();
-    let result = process(&file);
-    println!("{result}")
+    let (part1, part2) = process(&file);
+    println!("Part 1: {part1}, Part 2: {part2}")
 }
 
 #[test]
@@ -92,5 +113,6 @@ $ ls
 5626152 d.ext
 7214296 k";
 
-    assert_eq!(process(INPUT), 95437);
+    let (size, _) = process(INPUT);
+    assert_eq!(size, 95437);
 }
