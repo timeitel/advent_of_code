f84791892ea931735c126bd5be069fe9966d326f
Tim Eitel <timeitel@outlook.com>
2023-03-11 10:05:26 +0800

    feat(07): DANG IT WORKS THAT'S INSANE

--- 2022/rust/07/src/main.rs
+++ 2022/rust/07/src/main.rs
@@ -1,15 +1,63 @@
-use parsers::parse_commands;
+use parsers::*;
+use std::collections::HashMap;
 use std::fs;
 
 mod parsers;
 
-fn process(file: &str) -> usize {
+fn get_total_size_for_folder(paths: Vec<Path>) -> u32 {
+    paths.iter().fold(0, |acc, x| match x {
+        Path::File { name: _, size } => acc + *size,
+        _ => acc,
+    })
+}
+
+fn update_present_and_parent_dir_sizes(
+    dirs: &mut HashMap<String, u32>,
+    pwd: &Vec<&str>,
+    size: u32,
+) {
+    // dbg!(&dirs);
+    for i in 0..pwd.len() {
+        let dir = pwd[..=i].join("");
+        *dirs.entry(dir).or_insert(0) += size;
+    }
+    // dbg!(&dirs);
+}
+
+fn process(file: &str) -> u32 {
+    const MAX_DIR_SIZE: u32 = 100000;
     let (_, cmds) = parse_commands(file).unwrap();
 
-    let _pwd: Vec<&str> = vec![];
-    dbg!(cmds.len());
+    let mut pwd: Vec<&str> = vec![];
+    let mut dirs: HashMap<String, u32> = HashMap::new();
 
-    0
+    for c in cmds {
+        match c {
+            Command::Cd(Cd::Root) => {
+                pwd.clear();
+            }
+            Command::Cd(Cd::Up) => {
+                pwd.pop();
+            }
+            Command::Cd(Cd::Down(n)) => {
+                pwd.push(n);
+            }
+            Command::Ls(paths) => {
+                let size = get_total_size_for_folder(paths);
+                update_present_and_parent_dir_sizes(&mut dirs, &pwd, size);
+            }
+        }
+    }
+
+    let total_size = dirs.iter().fold(0, |acc, (_, size)| {
+        if *size <= MAX_DIR_SIZE {
+            acc + *size
+        } else {
+            acc
+        }
+    });
+
+    total_size
 }
 
 fn main() {
@@ -19,7 +67,6 @@ fn main() {
 }
 
 #[test]
-#[ignore]
 fn passes() {
     const INPUT: &str = "$ cd /
 $ ls
