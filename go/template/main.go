ad3250c9c32c1df65e09ec6def5de3767dd7bf63
Tim Eitel <timeitel@outlook.com>
2023-07-01 13:13:28 +1000

    feat: update template

--- go/template/main.go
+++ go/template/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
 )
 
-func process(str string) int {
-
-	return 0
-}
-
 func main() {
-	raw, _ := ioutil.ReadFile("input.txt")
-	content := string(raw)
+	content := read_file()
 
 	result := process(content)
 	fmt.Println(result)
+}
+
+func process(data []int) int {
+	return 0
+}
+
+func read_file() []int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	var fileLines []int
+
+	for fileScanner.Scan() {
+		text := fileScanner.Text()
+		number, err := strconv.Atoi(text)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileLines = append(fileLines, number)
+	}
+
+	file.Close()
+	return fileLines
 }
