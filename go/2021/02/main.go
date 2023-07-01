cee5d10345d1f10d2b675d9c4a1abb8d9bf08a1d
Tim Eitel <timeitel@outlook.com>
2023-07-01 13:13:41 +1000

    feat: 2021 02-01

---
+++ go/2021/02/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	content := read_file()
+
+	result := process(content)
+	fmt.Println(result)
+}
+
+func process(data []string) int {
+	var ins []Instruction
+
+	for _, rawIns := range data {
+		split := strings.Split(rawIns, " ")
+		dir := split[0]
+		val, _ := strconv.Atoi(split[1])
+		ins = append(ins, Instruction{dir, val})
+	}
+
+	h := 0
+	d := 0
+
+	for _, instruction := range ins {
+		switch instruction.dir {
+		case "forward":
+			h += instruction.val
+		case "up":
+			d -= instruction.val
+		case "down":
+			d += instruction.val
+		default:
+			log.Fatal(instruction)
+		}
+	}
+
+	return h * d
+}
+
+type Instruction struct {
+	dir string
+	val int
+}
+
+func read_file() []string {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	var fileLines []string
+
+	for fileScanner.Scan() {
+		text := fileScanner.Text()
+		fileLines = append(fileLines, text)
+	}
+
+	file.Close()
+	return fileLines
+}
