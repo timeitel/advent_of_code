86061c4d00c36a0c9c47fe23462ba75511d64c1b
Tim Eitel <timeitel@outlook.com>
2023-08-05 16:55:48 +1000

    feat(14): add tests

--- rust/2022/15/src/main.rs
+++ rust/2022/15/src/main.rs
@@ -54,7 +54,7 @@ fn tree_map(input: &str) -> IResult<&str, BTreeMap<Sensor, Beacon>> {
     ))
 }
 
-fn process_part1(input: &str) -> usize {
+fn process_part1(input: &str) -> String {
     let (_, tree_map) = tree_map(input).unwrap();
     let distances: BTreeMap<&Sensor, i32> = tree_map
         .iter()
@@ -86,4 +86,41 @@ fn process_part1(input: &str) -> usize {
             })
         })
         .count()
+        .to_string()
+}
+
+// TODO
+fn process_part2(input: &str) -> String {
+    "asd".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3";
+
+    #[test]
+    fn part_1_works() {
+        debug_assert_eq!(process_part1(INPUT), "26");
+    }
+
+    #[test]
+    #[ignore]
+    fn part_2_works() {
+        debug_assert_eq!(process_part2(INPUT), "26");
+    }
 }
